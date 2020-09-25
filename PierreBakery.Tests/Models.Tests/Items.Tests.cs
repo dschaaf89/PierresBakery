a167141438d5a05c217651e66ab1ca8011ba1c95
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-25 08:23:29 -0700

    write first unit test

--- PierreBakery.Tests/Models.Tests/Items.Tests.cs
+++ PierreBakery.Tests/Models.Tests/Items.Tests.cs
@@ -1,3 +1,18 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierreBakery.Models;
+
+namespace PierreBakery.Tests
+
+{
+    [TestClass]
+    public class ItemTests
+    {
+      
+      [TestMethod]
+      public void BreadConstructor_createsInstanceOfBread_Bread(){
+        Bread newBread = new Bread();
+        Assert.AreEqual(typeof(Bread),newBread.GetType());
+      }
+    }
+}
