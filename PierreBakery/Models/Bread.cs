f28427c65d6d336f980d3cdf2643244d467e106f
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-25 12:18:31 -0700

    rearrage my code into more files to seperate code. each class has its own cs file now

--- PierreBakery/Models/Bread.cs
+++ PierreBakery/Models/Bread.cs
@@ -27,25 +27,5 @@ namespace PierreBakery.Models {
 
   }
 
-  public class Pastry : Bakery{
-    
-     public int getTotal () {
-      int totalCost = 0;
-      totalCost = (AmtPastry * Price);
-      if (AmtPastry == 3) {
-        totalCost= 5;
-      }else if( AmtPastry % 3 == 0){
-        totalCost = (AmtPastry/3)*5;
-       
-      }
-      
-      return totalCost;
-    }
-    public int Price { get; set; }
-    public int AmtPastry { get; set; }
-    public Pastry (int amtPastry) {
-      AmtPastry = amtPastry;
-      Price = 2;
-    }
-  }
+  
 }
